140c7b65212360b5ec8263250fe008b4a91503ca
PeriodicLaw <periodiclaw0120@gmail.com>
2020-06-05 10:58:58 +0800

    improve prompt and lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,12 +3,19 @@ use std::iter::Peekable;
 use peeking_take_while::PeekableExt;
 
 #[derive(Debug)]
+pub enum Op {
+    Add, Minus, Mult, Divide, // + - * /
+    Eq, NotEq, Less, LessEq, Gre, GreEq, // == != < <= > >=
+    And, Or, Not, // && || !
+    Lambda, To // \ ->
+}
+
+#[derive(Debug)]
 pub enum Token{
     Identifier(String),
     KeyWord(String),
-    Symbol(char),
-    To, // ->
-    Lambda, // \ or lambda
+    Symbol(char), // ( ) [ ] = : , |
+    Op(Op),
     Int(i32),
     Bool(bool)
 }
@@ -31,32 +38,104 @@ impl<'a> Iterator for TokenStream<'a>{
         loop {
             return match self.chars.peek() {
                 Some(c) => match c{
-                    ' ' | '\t' => {
+                    ' ' | '\t' | '\n' => {
                         self.chars.next();
                         continue
+                    }
+                    
+                    '+' => {
+                        self.chars.next();
+                        Some(Token::Op(Op::Add))
                     }
                     '-' => {
                         self.chars.next();
                         match self.chars.peek() {
                             Some('>') => {
                                 self.chars.next();
-                                Some(Token::To)
+                                Some(Token::Op(Op::To))
                             }
-                            _ => Some(Token::Symbol('-'))
+                            _ => Some(Token::Op(Op::Minus))
                         }
                     }
-                    '+' | '*' | '/' | '[' | ']' | '(' | ')' | '|' | '=' | ':' | ',' | '.' => Some(Token::Symbol(self.chars.next().unwrap())),
+                    '*' => {
+                        self.chars.next();
+                        Some(Token::Op(Op::Mult))
+                    }
+                    '/' => {
+                        self.chars.next();
+                        Some(Token::Op(Op::Divide))
+                    }
+                    '<' => {
+                        self.chars.next();
+                        match self.chars.peek() {
+                            Some('=') => {
+                                self.chars.next();
+                                Some(Token::Op(Op::LessEq))
+                            }
+                            _ => Some(Token::Op(Op::Less))
+                        }
+                    }
+                    '>' => {
+                        self.chars.next();
+                        match self.chars.peek() {
+                            Some('=') => {
+                                self.chars.next();
+                                Some(Token::Op(Op::GreEq))
+                            }
+                            _ => Some(Token::Op(Op::Gre))
+                        }
+                    }
+                    '=' => {
+                        self.chars.next();
+                        match self.chars.peek() {
+                            Some('=') => {
+                                self.chars.next();
+                                Some(Token::Op(Op::Eq))
+                            }
+                            _ => Some(Token::Symbol('='))
+                        }
+                    }
+                    '&' => {
+                        self.chars.next();
+                        if self.chars.next() != Some('&') {
+                            println!("lexer: expected &&, but found single &");
+                            None
+                        } else {
+                            Some(Token::Op(Op::And))
+                        }
+                    }
+                    '|' => {
+                        self.chars.next();
+                        match self.chars.peek() {
+                            Some('|') => {
+                                self.chars.next();
+                                Some(Token::Op(Op::Or))
+                            }
+                            _ => Some(Token::Symbol('|'))
+                        }
+                    }
+                    '!' => {
+                        self.chars.next();
+                        match self.chars.peek() {
+                            Some('=') => {
+                                self.chars.next();
+                                Some(Token::Op(Op::NotEq))
+                            }
+                            _ => Some(Token::Op(Op::Not))
+                        }
+                    }
                     '\\' => {
                         self.chars.next();
-                        Some(Token::Lambda)
+                        Some(Token::Op(Op::Lambda))
                     }
-                    ';' | '\n' => None,
+                    
+                    '[' | ']' | '(' | ')' | ':' | ',' | '.' => Some(Token::Symbol(self.chars.next().unwrap())),
                     'a'..='z' | 'A'..='Z' | '_' => {
                         let s: String = self.chars.by_ref().peeking_take_while(
                             |c|{('a'..='z').contains(c) || ('A'..='Z').contains(c) || ('0'..'9').contains(c) || *c == '_'}
                         ).collect();
                         match &s[..] {
-                            "lambda" => Some(Token::Lambda),
+                            "lambda" => Some(Token::Op(Op::Lambda)),
                             "let" | "fix" | "Int" | "Bool" => Some(Token::KeyWord(s)),
                             "true" => Some(Token::Bool(true)),
                             "false" => Some(Token::Bool(false)),
@@ -66,7 +145,11 @@ impl<'a> Iterator for TokenStream<'a>{
                     '0'..='9' => {
                         Some(Token::Int(self.chars.by_ref().peeking_take_while(|c|{('0'..='9').contains(c)}).collect::<String>().parse::<i32>().unwrap()))
                     }
-                    _ => panic!("")
+                    ';' => None,
+                    _ => {
+                        println!("lexer: unrecognized char {}", c);
+                        None
+                    }
                 }
                 None => None
             }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,27 @@ fn main() -> io::Result<()> {
     let interface = Interface::new("MyFP")?;
     interface.set_prompt("> ")?;
     
-    while let ReadResult::Input(input) = interface.read_line()? {
-        let token_stream = lexer::TokenStream::new(&input);
-        for token in token_stream{
-            print!("{:?}, ", token);
+    let mut input: String = String::new();
+    
+    while let ReadResult::Input(line) = interface.read_line()? {
+        if !line.trim().is_empty() {
+            interface.add_history_unique(line.clone());
         }
-        println!();
+        
+        if line.trim().ends_with(';') {
+            input.push_str(&line[..]);
+            let token_stream = lexer::TokenStream::new(&input);
+            for token in token_stream{
+                print!("{:?}, ", token);
+            }
+            println!();
+            input.clear();
+            interface.set_prompt("> ")?;
+        } else {
+            input.push_str(&line[..]);
+            interface.set_prompt("  ")?;
+        }
+        
     }
     
     Ok(())
