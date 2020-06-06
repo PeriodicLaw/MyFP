904fb5de3584666a252b8f40ad4434f052049e64
PeriodicLaw <periodiclaw0120@gmail.com>
2020-06-07 03:05:17 +0800

    format

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,14 +1,30 @@
-use std::str::Chars;
-use std::iter::{Enumerate, Peekable};
 use itertools::Itertools;
+use std::iter::{Enumerate, Peekable};
+use std::str::Chars;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Op {
-    Add, Minus, Mult, Divide, // + - * /
-    Eq, NotEq, Less, LessEq, Gre, GreEq, // == != < <= > >=
-    And, Or, Not, // && || !
-    Cons, // ++
-    Lambda, To, CaseTo // \ -> =>
+    // + - * /
+    Add,
+    Minus,
+    Mult,
+    Divide,
+    // == != < <= > >=
+    Eq,
+    NotEq,
+    Less,
+    LessEq,
+    Gre,
+    GreEq,
+    // && || ! ++
+    And,
+    Or,
+    Not,
+    Cons,
+    // \ -> =>
+    Lambda,
+    To,
+    CaseTo,
 }
 
 impl Op {
@@ -20,10 +36,10 @@ impl Op {
             Op::Cons => Some(3),
             Op::Add | Op::Minus => Some(4),
             Op::Mult | Op::Divide => Some(5),
-            _ => None
+            _ => None,
         }
     }
-    
+
     pub fn to_str(&self) -> &str {
         match self {
             Op::Add => "+",
@@ -42,18 +58,26 @@ impl Op {
             Op::Cons => "++",
             Op::Lambda => "λ",
             Op::To => "->",
-            Op::CaseTo => "=>"
+            Op::CaseTo => "=>",
         }
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum KeyWord {
-    Int, Bool,
-    If, Then, Else,
-    Union, Case, Of,
-    Nil, Head, Tail,
-    Fix, Let
+    Int,
+    Bool,
+    If,
+    Then,
+    Else,
+    Union,
+    Case,
+    Of,
+    Nil,
+    Head,
+    Tail,
+    Fix,
+    Let,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -64,32 +88,33 @@ pub enum Token {
     Op(Op),
     Int(i32),
     Bool(bool),
-    End, Err
+    End,
+    Err,
 }
 pub struct TokenStream<'a> {
-    chars: Peekable<Enumerate<Chars<'a>>>
+    chars: Peekable<Enumerate<Chars<'a>>>,
 }
 
 impl TokenStream<'_> {
     pub fn new<'a>(str: &'a String) -> TokenStream<'a> {
-        TokenStream{
-            chars: str.chars().enumerate().peekable()
+        TokenStream {
+            chars: str.chars().enumerate().peekable(),
         }
     }
 }
 
-impl Iterator for TokenStream<'_>{
+impl Iterator for TokenStream<'_> {
     type Item = (usize, Token); // 第一分量为该token对应到input的位置
-    
+
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             return match self.chars.peek() {
-                Some((_, c)) => match c{
+                Some((_, c)) => match c {
                     ' ' | '\t' => {
                         self.chars.next();
-                        continue
+                        continue;
                     }
-                    
+
                     '+' => {
                         let p = self.chars.next()?.0;
                         match self.chars.peek() {
@@ -97,7 +122,7 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::Cons)))
                             }
-                            _ => Some((p, Token::Op(Op::Add)))
+                            _ => Some((p, Token::Op(Op::Add))),
                         }
                     }
                     '-' => {
@@ -107,7 +132,7 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::To)))
                             }
-                            _ => Some((p, Token::Op(Op::Minus)))
+                            _ => Some((p, Token::Op(Op::Minus))),
                         }
                     }
                     '*' => Some((self.chars.next()?.0, Token::Op(Op::Mult))),
@@ -119,7 +144,7 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::LessEq)))
                             }
-                            _ => Some((p, Token::Op(Op::Less)))
+                            _ => Some((p, Token::Op(Op::Less))),
                         }
                     }
                     '>' => {
@@ -129,7 +154,7 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::GreEq)))
                             }
-                            _ => Some((p, Token::Op(Op::Gre)))
+                            _ => Some((p, Token::Op(Op::Gre))),
                         }
                     }
                     '=' => {
@@ -143,7 +168,7 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::CaseTo)))
                             }
-                            _ => Some((p, Token::Symbol('=')))
+                            _ => Some((p, Token::Symbol('='))),
                         }
                     }
                     '&' => {
@@ -162,7 +187,7 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::Or)))
                             }
-                            _ => Some((p, Token::Symbol('|')))
+                            _ => Some((p, Token::Symbol('|'))),
                         }
                     }
                     '!' => {
@@ -172,55 +197,75 @@ impl Iterator for TokenStream<'_>{
                                 self.chars.next();
                                 Some((p, Token::Op(Op::NotEq)))
                             }
-                            _ => Some((p, Token::Op(Op::Not)))
+                            _ => Some((p, Token::Op(Op::Not))),
                         }
                     }
                     '\\' => Some((self.chars.next()?.0, Token::Op(Op::Lambda))),
-                    
+
                     '[' | ']' | '(' | ')' | ':' | ',' | '.' => {
                         let (p, c) = self.chars.next()?;
                         Some((p, Token::Symbol(c)))
                     }
                     'a'..='z' | 'A'..='Z' | '_' => {
                         let p = self.chars.peek()?.0;
-                        let s: String = self.chars.by_ref().peeking_take_while(|x|{
-                            let ref c = x.1;
-                            ('a'..='z').contains(c) || ('A'..='Z').contains(c) || ('0'..'9').contains(c) || *c == '_'
-                        }).map(|x|{x.1}).collect();
-                        Some((p, match &s[..] {
-                            "lambda" => Token::Op(Op::Lambda),
-                            "Int" => Token::KeyWord(KeyWord::Int),
-                            "Bool" => Token::KeyWord(KeyWord::Bool),
-                            "if" => Token::KeyWord(KeyWord::If),
-                            "then" => Token::KeyWord(KeyWord::Then),
-                            "else" => Token::KeyWord(KeyWord::Else),
-                            "union" => Token::KeyWord(KeyWord::Union),
-                            "case" => Token::KeyWord(KeyWord::Case),
-                            "of" => Token::KeyWord(KeyWord::Of),
-                            "nil" => Token::KeyWord(KeyWord::Nil),
-                            "head" => Token::KeyWord(KeyWord::Head),
-                            "tail" => Token::KeyWord(KeyWord::Tail),
-                            "fix" => Token::KeyWord(KeyWord::Fix),
-                            "let" => Token::KeyWord(KeyWord::Let),
-                            "true" => Token::Bool(true),
-                            "false" => Token::Bool(false),
-                            _ => Token::Identifier(s),
-                        }))
+                        let s: String = self
+                            .chars
+                            .by_ref()
+                            .peeking_take_while(|x| {
+                                let ref c = x.1;
+                                ('a'..='z').contains(c)
+                                    || ('A'..='Z').contains(c)
+                                    || ('0'..'9').contains(c)
+                                    || *c == '_'
+                            })
+                            .map(|x| x.1)
+                            .collect();
+                        Some((
+                            p,
+                            match &s[..] {
+                                "lambda" => Token::Op(Op::Lambda),
+                                "Int" => Token::KeyWord(KeyWord::Int),
+                                "Bool" => Token::KeyWord(KeyWord::Bool),
+                                "if" => Token::KeyWord(KeyWord::If),
+                                "then" => Token::KeyWord(KeyWord::Then),
+                                "else" => Token::KeyWord(KeyWord::Else),
+                                "union" => Token::KeyWord(KeyWord::Union),
+                                "case" => Token::KeyWord(KeyWord::Case),
+                                "of" => Token::KeyWord(KeyWord::Of),
+                                "nil" => Token::KeyWord(KeyWord::Nil),
+                                "head" => Token::KeyWord(KeyWord::Head),
+                                "tail" => Token::KeyWord(KeyWord::Tail),
+                                "fix" => Token::KeyWord(KeyWord::Fix),
+                                "let" => Token::KeyWord(KeyWord::Let),
+                                "true" => Token::Bool(true),
+                                "false" => Token::Bool(false),
+                                _ => Token::Identifier(s),
+                            },
+                        ))
                     }
-                    '0'..='9' => {
-                        Some((self.chars.peek()?.0, Token::Int(self.chars.by_ref().peeking_take_while(|x|{
-                            let ref c = x.1;
-                            ('0'..='9').contains(c)
-                        }).map(|x|{x.1}).collect::<String>().parse::<i32>().unwrap())))
-                    }
+                    '0'..='9' => Some((
+                        self.chars.peek()?.0,
+                        Token::Int(
+                            self.chars
+                                .by_ref()
+                                .peeking_take_while(|x| {
+                                    let ref c = x.1;
+                                    ('0'..='9').contains(c)
+                                })
+                                .map(|x| x.1)
+                                .collect::<String>()
+                                .parse::<i32>()
+                                .unwrap(),
+                        ),
+                    )),
                     ';' => Some((self.chars.next()?.0, Token::End)),
                     _ => {
                         eprintln!("lexer: unrecognized char {}", c);
                         Some((self.chars.peek()?.0, Token::Err))
                     }
-                }
-                None => None
-            }
+                },
+                None => None,
+            };
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 #![feature(box_patterns)]
-mod lexer;
+
 mod ast;
+mod lexer;
 mod parser;
 
-use std::io;
 use linefeed::{Interface, ReadResult};
+use std::io;
 
 fn main() -> io::Result<()> {
     let interface = Interface::new("MyFP")?;
     interface.set_prompt("> ")?;
-    
+
     let mut input: String = String::new();
-    
+
     while let ReadResult::Input(line) = interface.read_line()? {
         if !line.trim().is_empty() {
             interface.add_history_unique(line.clone());
         }
-        
+
         input.push_str(&line[..]);
         if line.trim().ends_with(';') {
             let mut parser = parser::Parser::new(&input);
@@ -25,11 +26,11 @@ fn main() -> io::Result<()> {
                 Err(pos) => {
                     let pos = match pos {
                         Some(pos) => pos,
-                        None => input.len() // 如果token流已经结束，我们简单地把错误提示放到最末尾
+                        None => input.len(), // 如果token流已经结束，我们简单地把错误提示放到最末尾
                     };
-                    
-                    let pos0 = if pos<5 {pos} else {5};
-                    let near: String = input.chars().skip(pos-pos0).take(pos0+10).collect();
+
+                    let pos0 = if pos < 5 { pos } else { 5 };
+                    let near: String = input.chars().skip(pos - pos0).take(pos0 + 10).collect();
                     eprintln!("error occurs at: {}", near);
                     eprintln!("                 {}^", " ".repeat(pos0));
                 }
@@ -40,8 +41,7 @@ fn main() -> io::Result<()> {
             input.push(' '); // 对于换行，我们手动给两行之间加上空格
             interface.set_prompt("  ")?;
         }
-        
     }
-    
+
     Ok(())
 }
